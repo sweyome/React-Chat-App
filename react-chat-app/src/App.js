88c50310d96129715061edc29288fe849ed5bef5
sweyome <sweyome1@gmail.com>
2021-04-27 19:32:46 -0400

    start up '

---
+++ react-chat-app/src/App.js
@@ -0,0 +1,16 @@
+import {ChatEngine, chatEngine} from 'react-chat-engine';
+
+import './App.css';
+
+const App = () =>{
+    return (
+        <ChatEngine  // add probs
+            height = "140vh"
+            projectID = "8d6ff8b4-65c7-4232-b369-bd4c61e03f4a"
+            userName =  "sweyome"
+            userSecret = "Tsinat20!"
+        />
+    );
+}
+
+export default App;
