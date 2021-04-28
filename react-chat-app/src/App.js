ee2a149db31661214edc7ce791a2d1732b81dcaa
sweyome <sweyome1@gmail.com>
2021-04-27 21:07:19 -0400

    chat egnie start

--- react-chat-app/src/App.js
+++ react-chat-app/src/App.js
@@ -1,6 +1,7 @@
-import {ChatEngine, chatEngine} from 'react-chat-engine';
+import {ChatEngine} from 'react-chat-engine';
 
 import './App.css';
+import ChatFeed from './components/ChatFeed.jsx';
 
 const App = () =>{
     return (
@@ -9,6 +10,7 @@ const App = () =>{
             projectID = "8d6ff8b4-65c7-4232-b369-bd4c61e03f4a"
             userName =  "sweyome"
             userSecret = "Tsinat20!"
+            rederChatFeed = {(chatAppProps) => <ChatFeed {... chatAppProps} />}
         />
     );
 }
---
+++ react-chat-app/src/components/ChatFeed.jsx
@@ -0,0 +1,11 @@
+const ChatFeed = (props) =>{
+    console.log(props)
+
+    return (
+        <div>
+            <p>Chat Feed</p>
+        </div>
+    )
+}
+
+export default ChatFeed
